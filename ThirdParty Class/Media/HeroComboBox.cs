d809acf31abbcf31f5643d23515c978efcf3faae
Hackerprod <hackerprodlive@gmail.com>
2021-03-15 14:15:00 -0400

    Add files via upload

---
+++ ThirdParty Class/Media/HeroComboBox.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HeroItems
+{
+    [DefaultEvent("SelectedIndexChanged")]
+    class HeroComboBox : ComboBox
+    {
+
+        private int X;
+        public HeroComboBox() : base()
+        {
+            TextChanged += GhostCombo_TextChanged;
+            DropDownClosed += GhostComboBox_DropDownClosed;
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.ResizeRedraw | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            DrawMode = DrawMode.OwnerDrawFixed;
+            ItemHeight = 18;
+            BackColor = Color.FromArgb(43, 47, 48);
+            DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+
+
+        protected override void OnMouseMove(System.Windows.Forms.MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            X = e.X;
+            Invalidate();
+        }
+
+        protected override void OnMouseLeave(System.EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            X = -1;
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (!(DropDownStyle == ComboBoxStyle.DropDownList))
+                DropDownStyle = ComboBoxStyle.DropDownList;
+            Bitmap B = new Bitmap(Width, Height);
+            Graphics G = Graphics.FromImage(B);
+
+            G.Clear(Color.FromArgb(43, 47, 48));
+            LinearGradientBrush GradientBrush = new LinearGradientBrush(new Rectangle(0, 0, Width, Height / 5 * 2), Color.FromArgb(63, 67, 68), Color.FromArgb(43, 47, 48), 90f);
+            G.FillRectangle(GradientBrush, new Rectangle(0, 0, Width, Height / 5 * 2));
+            HatchBrush hatch = default(HatchBrush);
+
+            int S1 = (int)G.MeasureString("OrainsComboBox", Font).Height;
+            if (SelectedIndex != -1)
+            {
+                G.DrawString(Items[SelectedIndex].ToString(), Font, new SolidBrush(ForeColor), 4, Height / 2 - S1 / 2);
+            }
+            else
+            {
+                if ((Items != null) & Items.Count > 0)
+                {
+                    G.DrawString(Items[0].ToString(), Font, new SolidBrush(ForeColor), 4, Height / 2 - S1 / 2);
+                }
+                else
+                {
+                    G.DrawString("OrainsComboBox", Font, new SolidBrush(ForeColor), 4, Height / 2 - S1 / 2);
+                }
+            }
+
+            if (MouseButtons == MouseButtons.None & X > Width - 25)
+            {
+                //G.FillRectangle(new SolidBrush(Color.FromArgb(7, Color.White)), Width - 25, 1, Width - 25, Height - 3);
+                  //ElseIf MouseButtons = Windows.Forms.MouseButtons.None and X < Width - 25 And X >= 0 Then
+                G.FillRectangle(new SolidBrush(Color.FromArgb(7, Color.Red)), 2, 1, Width - 5, Height - 3);
+            }
+
+            G.DrawRectangle(Pens.Black, 0, 0, Width - 1, Height - 1);
+            G.DrawRectangle(new Pen(Color.FromArgb(40, 40, 40)), 1, 1, Width - 3, Height - 3);
+            //G.DrawLine(New Pen(Color.FromArgb(40, 40, 40)), Width - 25, 1, Width - 25, Height - 3)
+            //G.DrawLine(Pens.Black, Width - 24, 0, Width - 24, Height)
+            // G.DrawLine(New Pen(Color.FromArgb(40, 40, 40)), Width - 23, 1, Width - 23, Height - 3)
+
+            G.FillPolygon(Brushes.Black, Triangle(new Point(Width - 14, Height / 2), new Size(5, 3)));
+            G.FillPolygon(Brushes.White, Triangle(new Point(Width - 15, Height / 2 - 1), new Size(5, 3)));
+
+            e.Graphics.DrawImage((Bitmap)B.Clone(), 0, 0);
+            G.Dispose();
+            B.Dispose();
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            if (e.Index < 0)
+                return;
+            Rectangle rect = new Rectangle();
+            rect.X = e.Bounds.X;
+            rect.Y = e.Bounds.Y;
+            rect.Width = e.Bounds.Width - 1;
+            rect.Height = e.Bounds.Height - 1;
+
+            e.DrawBackground();
+            if ((int)e.State == 785 | (int)e.State == 17)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(BackColor), e.Bounds);
+                Rectangle x2 = new Rectangle(e.Bounds.Location, new Size(e.Bounds.Width + 2, e.Bounds.Height));
+                Rectangle x3 = new Rectangle(x2.Location, new Size(x2.Width, (x2.Height / 2) - 1));
+                LinearGradientBrush G1 = new LinearGradientBrush(new Point(x2.X, x2.Y), new Point(x2.X, x2.Y + x2.Height), Color.FromArgb(63, 67, 68), Color.FromArgb(23, 27, 28));//Color mausemove
+                HatchBrush H = new HatchBrush(HatchStyle.DarkUpwardDiagonal, Color.FromArgb(63, 67, 68), Color.Transparent); 
+                e.Graphics.FillRectangle(G1, x2);
+                G1.Dispose();
+                e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(43, 47, 48)), x3);
+                e.Graphics.FillRectangle(H, x2);
+                G1.Dispose();
+                e.Graphics.DrawString(" " + Items[e.Index].ToString(), Font, Brushes.White, e.Bounds.X, e.Bounds.Y + 1); //ForeColor mousemove
+            }
+            else
+            {
+                e.Graphics.FillRectangle(new SolidBrush(BackColor), e.Bounds);
+                e.Graphics.DrawString(" " + Items[e.Index].ToString(), Font, Brushes.Gray, e.Bounds.X, e.Bounds.Y + 1);
+
+            }
+
+            base.OnDrawItem(e);
+        }
+
+        public Point[] Triangle(Point Location, Size Size)
+        {
+            Point[] ReturnPoints = new Point[4];
+            ReturnPoints[0] = Location;
+            ReturnPoints[1] = new Point(Location.X + Size.Width, Location.Y);
+            ReturnPoints[2] = new Point(Location.X + Size.Width / 2, Location.Y + Size.Height);
+            ReturnPoints[3] = Location;
+
+            return ReturnPoints;
+        }
+
+        private void GhostComboBox_DropDownClosed(object sender, System.EventArgs e)
+        {
+            DropDownStyle = ComboBoxStyle.Simple;
+            Application.DoEvents();
+            DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void GhostCombo_TextChanged(object sender, System.EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HeroComboBox
+            // 
+            this.SelectedIndexChanged += new System.EventHandler(this.HeroComboBox_SelectedIndexChanged);
+            this.ResumeLayout(false);
+
+        }
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            Invalidate();
+        }
+        private void HeroComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            base.SelectedIndexChanged += HeroComboBox_SelectedIndexChanged;
+            Invalidate();
+        }
+    }
+
+}
---
+++ ThirdParty Class/Media/Sound.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+public class SoundEx
+{
+    private readonly Timer timer1;
+
+    public bool FileIsOpen;
+
+    private string sCommand;
+
+    private readonly StringBuilder sBuffer = new StringBuilder(128);
+
+    private bool m_Repeat;
+
+    private bool m_Pause;
+
+    private int seconds;
+
+    public bool Pause
+    {
+        get
+        {
+            return m_Pause;
+        }
+        set
+        {
+            m_Pause = value;
+            if (m_Pause)
+            {
+                sCommand = "pause MediaFile";
+            }
+            else
+            {
+                sCommand = "resume MediaFile";
+            }
+            mciSendString(sCommand, null, 0, IntPtr.Zero);
+        }
+    }
+
+    public int Position
+    {
+        get
+        {
+            sCommand = "status MediaFile position";
+            mciSendString(sCommand, sBuffer, sBuffer.Capacity, IntPtr.Zero);
+            try
+            {
+                seconds = int.Parse(sBuffer.ToString());
+                seconds /= 1000;
+
+            }
+            catch { }
+            return seconds;
+        }
+        set
+        {
+            seconds = value;
+            seconds *= 1000;
+            sCommand = "play MediaFile from " + seconds.ToString();
+            mciSendString(sCommand, null, 0, IntPtr.Zero);
+        }
+    }
+
+    public int Volume
+    {
+        get
+        {
+            if (Status() != "")
+            {
+                mciSendString("status MediaFile volume", sBuffer, sBuffer.Capacity, IntPtr.Zero);
+                return int.Parse(sBuffer.ToString());
+            }
+            return 0;
+        }
+        set
+        {
+            if (value <= 1000)
+            {
+                mciSendString("setaudio MediaFile volume to " + value.ToString(), null, 0, IntPtr.Zero);
+            }
+            else
+            {
+                MessageBox.Show("Volume value must be smaller than 1000");
+            }
+        }
+    }
+
+    public bool Repeat
+    {
+        get
+        {
+            return m_Repeat;
+        }
+        set
+        {
+            m_Repeat = value;
+        }
+    }
+
+    public event PositionEventHandler PositionChanged;
+
+    [DllImport("winmm.dll")]
+    private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+    [DllImport("Winmm.dll")]
+    private static extern long PlaySound(byte[] data, IntPtr hMod, uint dwFlags);
+
+    public SoundEx()
+    {
+        timer1 = new Timer();
+        timer1.Enabled = false;
+        timer1.Interval = 100;
+        timer1.Tick += timer1_Tick;
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        PositionChangedEventArgs e2 = new PositionChangedEventArgs(Position);
+        PositionChanged?.Invoke(this, e2);
+    }
+
+    public void Close()
+    {
+        sCommand = "close MediaFile";
+        mciSendString(sCommand, null, 0, IntPtr.Zero);
+        timer1.Enabled = false;
+        FileIsOpen = false;
+    }
+
+    public void Stop()
+    {
+        Position = 0;
+        sCommand = "stop MediaFile";
+        mciSendString(sCommand, null, 0, IntPtr.Zero);
+        timer1.Enabled = false;
+    }
+
+    public void Open(string sFileName)
+    {
+        if (Status() == "playing")
+        {
+            Close();
+        }
+        sCommand = "open \"" + sFileName + "\" type mpegvideo alias MediaFile";
+        mciSendString(sCommand, null, 0, IntPtr.Zero);
+        FileIsOpen = true;
+    }
+
+    public void Open(string sFileName, PictureBox videobox)
+    {
+        if (Status() == "playing")
+        {
+            Close();
+        }
+        sCommand = "open \"" + sFileName + "\" type mpegvideo alias MediaFile style child parent " + videobox.Handle.ToInt32();
+        mciSendString(sCommand, null, 0, IntPtr.Zero);
+        sCommand = "put MediaFile window at 0 0 " + videobox.Width + " " + videobox.Height;
+        mciSendString(sCommand, null, 0, IntPtr.Zero);
+        FileIsOpen = true;
+    }
+
+    public void Play()
+    {
+        if (FileIsOpen)
+        {
+            sCommand = "play MediaFile";
+            if (Repeat)
+            {
+                sCommand += " REPEAT";
+            }
+            mciSendString(sCommand, null, 0, IntPtr.Zero);
+            timer1.Enabled = true;
+        }
+    }
+
+    public void FullScreen()
+    {
+        sCommand = "play MediaFile FullScreen";
+        mciSendString(sCommand, null, 0, IntPtr.Zero);
+    }
+
+    public int Duration()
+    {
+        sCommand = "status MediaFile length";
+        mciSendString(sCommand, sBuffer, 128, IntPtr.Zero);
+        int num = int.Parse(sBuffer.ToString());
+        return num / 1000;
+    }
+    public string Test()
+    {
+        sCommand = "status MediaFile mode";
+        mciSendString(sCommand, sBuffer, 128, IntPtr.Zero);
+        return sBuffer.ToString();
+    }
+    public string Status()
+    {
+        mciSendString("status MediaFile mode", sBuffer, sBuffer.Capacity, IntPtr.Zero);
+        return sBuffer.ToString();
+    }
+
+    public string SecondsToTime(int seconds)
+    {
+        int num = seconds / 3600;
+        int num2 = seconds / 60;
+        num2 %= 60;
+        seconds %= 60;
+        return num.ToString("00") + ":" + num2.ToString("00") + ":" + seconds.ToString("00");
+    }
+
+    public void PlayWavResource(string wav)
+    {
+        string str = Assembly.GetExecutingAssembly().GetName().Name;
+        Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(str + "." + wav);
+        if (manifestResourceStream != null)
+        {
+            byte[] array = new byte[manifestResourceStream.Length];
+            manifestResourceStream.Read(array, 0, (int)manifestResourceStream.Length);
+            PlaySound(array, IntPtr.Zero, 5u);
+        }
+    }
+}
