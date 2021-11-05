9a9bc4fca9c34a027b0ed23ac7cf9e99587e70cf
Hackerprod <hackerprodlive@gmail.com>
2021-11-04 20:44:05 -0400

    Update

--- SKYNET Dota2 Mods/Form1.Designer.cs
+++ SKYNET Dota2 Mods/Form1.Designer.cs
@@ -29,10 +29,18 @@ namespace SKYNET_Dota2_Mods
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
